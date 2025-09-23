9460c00a46b90b506f3de8afd9016f906694e5a6
ravneeetkaur <ravneet.kaur3@edu.sait.ca>
2025-09-23 01:09:00 -0600

    new commit

--- app/auth/signup.tsx
+++ app/auth/signup.tsx
@@ -4,6 +4,7 @@ import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'reac
 import { Link, useRouter } from 'expo-router';
 import { createUserWithEmailAndPassword, sendEmailVerification, signOut } from 'firebase/auth';
 import { auth } from '../../lib/firebase';
+import React from 'react';
 
 export default function Signup() {
   const router = useRouter();
